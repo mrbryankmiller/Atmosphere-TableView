21bad122affabc90b1b6fccf8d6e9ad9eecd8e4d
Bryan Miller <bryanmiller@bryans-mbp.attlocal.net>
2016-08-03 22:25:22 -0500

    Atmosphere TableView

---
+++ tableViews Practice/TableTableViewController.swift
@@ -0,0 +1,104 @@
+//
+//  TableTableViewController.swift
+//  tableViews Practice
+//
+//  Created by Bryan  Miller on 8/2/16.
+//  Copyright © 2016 youtube. All rights reserved.
+//
+
+import UIKit
+
+class TableTableViewController: UITableViewController {
+    
+   
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return atmosphereLayers.layers.count
+    }
+
+   
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        cell.textLabel!.text = atmosphereLayers.layers[indexPath.row]
+
+        return cell
+    }
+    
+
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+
+    
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            
+            atmosphereLayers.layers.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+
+    
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+        
+        let atmosphere = atmosphereLayers.layers [fromIndexPath.row]
+        atmosphereLayers.layers.removeAtIndex(fromIndexPath.row)
+        atmosphereLayers.layers.insert(atmosphere, atIndex: toIndexPath.row)
+
+    }
+
+
+    
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tableViews Practice/atmosphereLayers.swift
@@ -0,0 +1,13 @@
+//
+//  atmosphereLayers.swift
+//  tableViews Practice
+//
+//  Created by Bryan  Miller on 8/3/16.
+//  Copyright © 2016 youtube. All rights reserved.
+//
+
+import UIKit
+
+class atmosphereLayers: NSObject {
+    static var layers = ["Troposphere", "Stratosphere","Mesophere","Thermosphere"]
+}
